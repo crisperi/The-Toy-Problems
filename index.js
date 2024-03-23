062ae2dd31cb8683e2d7c77431d12a1bb64e5de0
crisperi <phoenixedian@gmail.com>
2024-03-24 00:52:12 +0300

    fixed the issue with forms labels

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Student Grade Generator 
+// will need input value for grade in html -- problem to solve 
+
+const form =document.getElementById('myForm');
+
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+
+    const grade = document.getElementById('grade');
+
+    console.log(grade);
+})
+
+
+
