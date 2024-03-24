045220df7555385b7a9eb43f9d5b86f162d9ec26
crisperi <phoenixedian@gmail.com>
2024-03-24 12:02:05 +0300

    added speedCalculator function

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const gradeResult = document.getElementById('gradeResults')
 form.addEventListener('click', function (event) {
     //prevent the browser from default
     event.preventDefault();
-// get grade value input 
+    // get grade value input 
     const grade = document.getElementById('grade').value;
 
     //a function to calculate the grade scores 
     function gradeEvaluator(grade) {
-        if (grade > 79 && grade <= 100 ) {
+        if (grade > 79 && grade <= 100) {
             return 'A';
 
         } else if (grade >= 60 && grade <= 79) {
@@ -44,5 +44,19 @@ form.addEventListener('click', function (event) {
 // get the speedDisplay div in html
 const speedDisplay = document.getElementById('speedDisplay');
 
+
+function speedCalculator(speed) {
+    if (speed <= 70) {
+        return "Ok";
+    } else if (speed > 70) {
+        excessSpeed = speed - 70;
+        dPoints = Math.floor(excessSpeed / 5);
+        if (dPoints < 12) {
+            return `you have ${dPoints} demerit points`
+        } else if (dPoints >= 12) {
+            return "your license is suspended !"
+        }
+    }
+}
 
 
