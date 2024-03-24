830f8c8950e1d367a7ad3f216898b22c8b209723
crisperi <phoenixedian@gmail.com>
2024-03-24 10:48:21 +0300

    added function gradeEvaluator

--- index.js
+++ index.js
@@ -13,4 +13,22 @@ form.addEventListener('click',function(event){
 })
 
 
+function gradeEvaluator (grade){
+    if (grade > 79){
+        return A;
+
+    }else if (grade >= 60 && grade <= 79){
+        return B ;
+    }else if (grade >=49 && grade <= 59){
+        return C;
+    }else if (grade >= 40 && grade <=49){
+        return D ; 
+    }else if (grade < 40){
+        return E ; 
+    }else {
+        const defaulMsg = "The input should be between 0 -100"
+        return defaulMsg;
+    }
+
+}
 
